05e1c9483ff7a658d0be1cede398a16e05d3e414
Shashi Kumar <krshashi2605@gmail.com>
2026-06-06 04:15:24 +0530

    fix server URL for production

--- client/src/services/socketService.js
+++ client/src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const SERVER_URL = "http://localhost:5000";
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
 
 let socket = null;
 
